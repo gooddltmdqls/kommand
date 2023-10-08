c16cc8448523e6cb6625d8775534cd1e5e737ebb
Icetang0123 <1415wwwh@gmail.com>
2023-10-08 14:47:21 +0900

    workflows update

--- kommand-api/build.gradle.kts
+++ kommand-api/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.dokka)
+    `maven-publish`
+    signing
 }
 
 tasks {
@@ -16,4 +18,63 @@ tasks {
             include("**")
         }
     }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("kommand-api") {
+            artifactId = "kommand-api"
+
+            from(project.components["java"])
+
+            if (hasProperty("dev")) {
+                artifact(project.tasks["devJar"])
+            }
+            artifact(projectApi.tasks["sourcesJar"])
+            artifact(projectApi.tasks["dokkaJar"])
+
+            pom {
+                name.set("kommand-api")
+                description.set("Kotlin DSL for PaperMC commands")
+                url.set("https://github.com/gooddltmdqls/${rootProject.name}")
+
+                licenses {
+                    license {
+                        name.set("GNU General Public License version 3")
+                        url.set("https://opensource.org/licenses/GPL-3.0")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("monun")
+                        name.set("Monun")
+                        email.set("monun1010@gmail.com")
+                        url.set("https://github.com/monun")
+                        roles.addAll("developer")
+                        timezone.set("Asia/Seoul")
+                    }
+                    developer {
+                        id.set("icetang0123")
+                        name.set("Icetang0123")
+                        email.set("1415wwwh@gmail.com")
+                        url.set("https://github.com/gooddltmdqls")
+                        roles.addAll("developer")
+                        timezone.set("Asia/Seoul")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/gooddltdmqls/${rootProject.name}.git")
+                    developerConnection.set("scm:git:ssh://github.com:gooddltmdqls/${rootProject.name}.git")
+                    url.set("https://github.com/gooddltmdqls/${rootProject.name}")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    isRequired = true
+    sign(publishing.publications["kommand-api"])
 }
--- kommand-core/build.gradle.kts
+++ kommand-core/build.gradle.kts
@@ -1,32 +1,49 @@
 plugins {
+    alias(libs.plugins.dokka)
     alias(libs.plugins.paperweight) apply false
+    `maven-publish`
+    signing
 }
 
 dependencies {
     implementation(projectApi)
 }
 
-//tasks {
-//    register<Jar>("devJar") {
-//        archiveClassifier.set("dev")
-//
-//        from(sourceSets["main"].output)
-//        subprojects {
-//            from(sourceSets["main"].output)
-//        }
-//    }
-//
-//    register<Jar>("reobfJar") {
-//        archiveClassifier.set("reobf")
-//
-//        from(sourceSets["main"].output)
-//        subprojects {
-//            val reobfJar = tasks.named("reobfJar").get() as io.papermc.paperweight.tasks.RemapJar
-//            dependsOn(reobfJar)
-//            from(zipTree(reobfJar.outputJar))
-//        }
-//    }
-//}
+tasks {
+    register<Jar>("devJar") {
+        archiveClassifier.set("dev")
+
+        from(sourceSets["main"].output)
+        subprojects {
+            from(sourceSets["main"].output)
+        }
+    }
+
+    register<Jar>("reobfJar") {
+        archiveClassifier.set("reobf")
+
+        from(sourceSets["main"].output)
+        subprojects {
+            val reobfJar = tasks.named("reobfJar").get() as io.papermc.paperweight.tasks.RemapJar
+            dependsOn(reobfJar)
+            from(zipTree(reobfJar.outputJar))
+        }
+    }
+
+    create<Jar>("sourcesJar") {
+        archiveClassifier.set("sources")
+        from(sourceSets["main"].allSource)
+    }
+
+    create<Jar>("dokkaJar") {
+        archiveClassifier.set("javadoc")
+        dependsOn("dokkaHtml")
+
+        from("$buildDir/dokka/html/") {
+            include("**")
+        }
+    }
+}
 
 subprojects {
     apply(plugin = rootProject.libs.plugins.paperweight.get().pluginId)
@@ -38,4 +55,64 @@ subprojects {
                 as io.papermc.paperweight.userdev.PaperweightUserDependenciesExtension
         paperweight.paperDevBundle("${name.substring(1)}-R0.1-SNAPSHOT")
     }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("kommand-core") {
+            artifactId = "kommand-core"
+
+            from(project.components["java"])
+
+            if (hasProperty("dev")) {
+                artifact(tasks["devJar"])
+            }
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["dokkaJar"])
+            artifact(tasks["reobfJar"])
+
+            pom {
+                name.set("kommand-core")
+                description.set("Kotlin DSL for PaperMC commands")
+                url.set("https://github.com/gooddltmdqls/${rootProject.name}")
+
+                licenses {
+                    license {
+                        name.set("GNU General Public License version 3")
+                        url.set("https://opensource.org/licenses/GPL-3.0")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("monun")
+                        name.set("Monun")
+                        email.set("monun1010@gmail.com")
+                        url.set("https://github.com/monun")
+                        roles.addAll("developer")
+                        timezone.set("Asia/Seoul")
+                    }
+                    developer {
+                        id.set("icetang0123")
+                        name.set("Icetang0123")
+                        email.set("1415wwwh@gmail.com")
+                        url.set("https://github.com/gooddltmdqls")
+                        roles.addAll("developer")
+                        timezone.set("Asia/Seoul")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/gooddltdmqls/${rootProject.name}.git")
+                    developerConnection.set("scm:git:ssh://github.com:gooddltmdqls/${rootProject.name}.git")
+                    url.set("https://github.com/gooddltmdqls/${rootProject.name}")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    isRequired = true
+    sign(publishing.publications["kommand-core"])
 }
